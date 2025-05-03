1f978b18ae84698293193569620c15274aadf9ff
Abrham <@Abrham503>
2025-05-03 16:12:35 +0300

    Service Controller

---
+++ src/main/java/com/example/basingspringboota2025/controller/PostRestController.java
@@ -0,0 +1,95 @@
+package com.example.basingspringboota2025.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("/api/post")
+@RestController
+public class PostRestController {
+
+    int order = 0;
+    List<Map<String, Object>> list = new ArrayList<>();
+
+    @RequestMapping("/create")
+    public Map<String, Object> create(@RequestParam Map<String, Object> map) {
+        if (map.get("title") == null ||(map.get("title") + "").isEmpty()){
+             throw new RuntimeException("no param!");
+        }
+        String title = (String) map.get("title");
+        String content = (String) map.get("content");
+        String author = (String) map.get("author");
+
+        Map<String, Object> post = new HashMap<>();
+        post.put("title", title);
+        post.put("content", content);
+        post.put("author", author);
+        post.put("id", ++order);
+        list.add(post);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("resultcode", 200);
+        result.put("id", order);
+
+        return result;
+    }
+
+    @RequestMapping("/update")
+    public void update(@RequestParam Map<String, Object> map) {
+        if (map.get("id") == null ||(map.get("id") + "").isEmpty()){
+            throw new RuntimeException("no id!");
+        }
+        int id = Integer.parseInt(map.get("id").toString());
+        Map<String, Object> post = detail(id);
+        post.put("title", map.get("title"));
+        post.put("content", map.get("content"));
+        post.put("author", map.get("author"));
+    }
+
+    @RequestMapping("/delete")
+    public void delete(@RequestParam int id) {
+        Map<String, Object> post = detail(id);
+        post.remove("title");
+        post.remove("content");
+        post.remove("author");
+    }
+
+    @RequestMapping("/detail/{id}")
+    public Map<String, Object> detail(@PathVariable int id) {
+        for(Map<String, Object> each : list){
+            int eachId = Integer.parseInt(each.get("id").toString());
+            if(eachId == id){
+            return each;
+            }
+        }
+        return null;
+    }
+
+    @RequestMapping("/list")
+    public List<Map<String, Object>> list(String title) {
+
+        //1. there is no search keyword
+        if (title == null || title.isEmpty()){
+            return list;
+        }
+
+        //2. keyword exist
+        List<Map<String, Object>> returnlist = new ArrayList<>();
+        for(Map<String, Object> each : list){
+            String eachTitle = each.get("title").toString();
+            if(eachTitle.contains(title)){
+                returnlist.add(each);
+            }
+        }
+
+
+        return returnlist;
+    }
+
+}
---
+++ src/main/java/com/example/basingspringboota2025/controller/page/PostpageController.java
@@ -0,0 +1,26 @@
+package com.example.basingspringboota2025.controller.page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/post")
+@Controller
+public class PostpageController {
+    /*
+    /post/create => create ---> String page!!
+    page's value ---> "create"   !!!
+    * */
+    @RequestMapping("/{page}")
+    public String page(@PathVariable  String page){
+        //pathVariable is some part of url can be stored in a variable
+        //if you specify the variable with @pathVarible annotation in advance
+        return "post/" + page;
+    }
+
+    @RequestMapping("/{page}/{id}")
+    public String page(@PathVariable String page,  @PathVariable  String id){
+        System.out.println("id: " + id);
+        return "post/" + page;
+    }
+}
